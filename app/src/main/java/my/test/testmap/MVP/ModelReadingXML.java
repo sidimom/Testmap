ff70d6fcce6ac7359c9e49d3cc24423291d7edd0
VE <sidimom@gmail.com>
2019-11-05 15:42:24 +0200

    delete unneeded code

--- app/src/main/java/my/test/testmap/MVP/ModelReadingXML.java
+++ app/src/main/java/my/test/testmap/MVP/ModelReadingXML.java
@@ -6,7 +6,6 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 import my.test.testmap.Region;
 
@@ -21,46 +20,6 @@ public class ModelReadingXML {
     public List<Region> getRegions() throws XmlPullParserException, IOException {
         List<Region> regions = new ArrayList<>();
         addRegion(parser, regions);
-        /*while (parser.getEventType() != XmlPullParser.END_DOCUMENT){
-
-            if (parser.getEventType() == XmlPullParser.START_TAG
-                    && parser.getName().equals("region")){
-
-                int countAttribute = parser.getAttributeCount();
-                Region newRegion = new Region();
-                for (int i = 0; i < countAttribute; i++) {
-                    switch (parser.getAttributeName(i)){
-                        case "name":
-                            newRegion.name = parser.getAttributeValue(i);
-                            break;
-                        case "map":
-                            newRegion.isDownloaded = parser.getAttributeValue(i).equals("yes");
-                            break;
-                        case "download_suffix":
-                            newRegion.download_suffix = parser.getAttributeValue(i);
-                            break;
-                        case "inner_download_suffix":
-                            newRegion.inner_download_suffix = parser.getAttributeValue(i);
-                            break;
-                        case "download_prefix":
-                            newRegion.download_prefix = parser.getAttributeValue(i);
-                            break;
-                        case "inner_download_prefix":
-                            newRegion.inner_download_prefix = parser.getAttributeValue(i);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                if (newRegion.inner_download_prefix != null
-                        && !newRegion.inner_download_prefix.isEmpty()){
-                    newRegion.subregions = addSubregions(parser, newRegion.name);
-                }
-                regions.add(newRegion);
-            }
-            parser.next();
-        }*/
-
         return regions;
     }
 
@@ -107,50 +66,5 @@ public class ModelReadingXML {
             }
             parser.next();
         }
-    }
-
-    private List<Region> addSubregions(XmlPullParser parser, String name) throws XmlPullParserException, IOException {
-        List<Region> subregions = new ArrayList<>();
-        Stack<String> stackSubRegions = new Stack<>();
-        stackSubRegions.push(name);
-        while (!stackSubRegions.empty()){
-            parser.next();
-            if (parser.getEventType() == XmlPullParser.END_TAG){
-                stackSubRegions.pop();
-            }else if (parser.getEventType() == XmlPullParser.START_TAG
-                    && parser.getName().equals("region")){
-
-                int countAttribute = parser.getAttributeCount();
-                Region newRegion = new Region();
-                for (int i = 0; i < countAttribute; i++) {
-                    switch (parser.getAttributeName(i)){
-                        case "name":
-                            newRegion.name = parser.getAttributeValue(i);
-                            break;
-                        case "map":
-                            newRegion.isDownloaded = parser.getAttributeValue(i).equals("yes");
-                            break;
-                        case "download_suffix":
-                            newRegion.download_suffix = parser.getAttributeValue(i);
-                            break;
-                        case "inner_download_suffix":
-                            newRegion.inner_download_suffix = parser.getAttributeValue(i);
-                            break;
-                        case "download_prefix":
-                            newRegion.download_prefix = parser.getAttributeValue(i);
-                            break;
-                        case "inner_download_prefix":
-                            newRegion.inner_download_prefix = parser.getAttributeValue(i);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-
-                stackSubRegions.push(newRegion.name);
-                subregions.add(newRegion);
-            }
-        }
-        return subregions;
     }
 }
